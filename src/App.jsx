67f4ecdf25f970fe023618a81c8bcac7cdc8b130
Dinda Maharani <143940529+Dindamaharn@users.noreply.github.com>
2026-02-26 15:47:25 +0700

    add user admin route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/user/Home";
 import HomeAdmin from "./pages/admin/HomeAdmin";
+import UsersAdmin from "./pages/admin/UsersAdmin";
+import DetailUsers from "./pages/admin/DetailUsers";
+import SubmissionDetail from "./pages/admin/SubmissionDetail";
 import Profile from "./pages/user/Profile";
 import Bookmark from "./pages/user/Bookmark";
 import History from "./pages/user/History";
@@ -16,9 +19,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<Splash />} />
+        {/* Public */}
+        <Route path="/" element={<Splash />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/* User */}
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/edit-profile" element={<EditProfile />} />
@@ -26,8 +32,13 @@ function App() {
         <Route path="/history" element={<History />} />
         <Route path="/shopping" element={<Shopping />} />
         <Route path="/recipes" element={<Recipes />} />
-        <Route path="/detail-recipes" element={<DetailRecipes />} />
+        <Route path="/recipes/:id" element={<DetailRecipes />} />
+
+        {/* Admin */}
         <Route path="/admin/home" element={<HomeAdmin />} />
+        <Route path="/admin/users" element={<UsersAdmin />} />
+        <Route path="/admin/users/:id" element={<DetailUsers />} />
+        <Route path="/admin/submissions/:id" element={<SubmissionDetail />} />
       </Routes>
     </BrowserRouter>
   );
